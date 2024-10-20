6480d5b7664644dac89a5117878adc4a09432f12
Samuel Ferreira <samferreira0000@gmail.com>
2024-10-20 20:26:54 -0300

    js up

--- IA-TITAS/static/js/Password.js
+++ IA-TITAS/static/js/Password.js
@@ -1,33 +1,57 @@
-var Password = document.getElementById('password')
-var ConfirmPassword = document.getElementById('confirm-password')
-var Error = document.getElementById('error')
-var SubmitButton = document.querySelector('button[type="submit"]')
 
+const Password = document.getElementById('password')
+const ConfirmPassword = document.getElementById('confirm-password')
+const Error = document.getElementById('error')
+const SubmitButton = document.querySelector('button[type="submit"]')
+const Nickname = document.getElementById('name')
+const showPasswordCheckbox = document.getElementById('show-password')
+var i = 0, u = 0, l = 0
 // Inicialmente, oculta a mensagem de erro
 Error.style.display = 'none'
 
-SubmitButton.addEventListener('click', function(event) {
+SubmitButton.addEventListener('click', function (event) {
     // Previne o envio do formulário
     event.preventDefault()
 
+    // Verifica se o nickname está vazio
+    if (Nickname.value.trim() === '') {
+        alert('Por favor, insira seu nome')
+    } else {
+        if (i < 1){
+            i++
+        }
+    }
+    // Verifica se as senhas estão vazias
+    if (Password.value.trim() === '' || ConfirmPassword.value.trim() === '') {
+        alert('Por favor, insira sua senha')
+    } else {
+        if (u < 1){
+            u++
+        }
+    }
     // Verifica se as senhas coincidem
     if (Password.value !== ConfirmPassword.value) {
         Error.style.display = 'block'
         Error.style.color = 'red'
     } else {
         Error.style.display = 'none' // Oculta a mensagem de erro
-        window.location.href = 'home.html' // Redireciona para home.html
+        if (l < 1){
+            l++
+        }
     }
+    // Se tudo estiver correto, redireciona
+    if (i == 1 && u == 1 && l == 1) {
+        window.location.href = 'home.html'
+    }
+
 })
 
-var showPasswordCheckbox = document.getElementById('show-password')
-
-showPasswordCheckbox.addEventListener('change', function() {
+showPasswordCheckbox.addEventListener('change', function () {
     if (showPasswordCheckbox.checked) {
-        Password.type = 'text'; // Muda o tipo para 'text' para mostrar a senha
-        ConfirmPassword.type = 'text'; // Também muda o tipo do campo de confirmação
+        Password.type = 'text' // Muda o tipo para 'text' para mostrar a senha
+        ConfirmPassword.type = 'text'
     } else {
-        Password.type = 'password'; // Volta para 'password' para ocultar a senha
-        ConfirmPassword.type = 'password'; // Também volta para 'password' no campo de confirmação
+        Password.type = 'password' // Volta para 'password' para ocultar a senha
+        ConfirmPassword.type = 'password'
     }
-});
+})
